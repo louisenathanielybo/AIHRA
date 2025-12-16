e551a25b88fb36a14d55c2a2b162a4dfcdb76e92
Christian Mendoza <chimen0531@gmail.com>
2025-12-16 14:10:02 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -134,7 +134,8 @@ Route::middleware(['auth'])->group(function () {
         
         // 🆕 ADD THIS ROUTE - Dialogflow intents management
         // Add this route to your existing Dialogflow routes
-        Route::post('/dialogflow/sync', [DialogflowController::class, 'syncIntents'])->name('admin.dialogflow.sync');
+        Route::post('/dialogflow/sync', [DialogflowController::class, 'syncFromDialogflow'])->name('admin.dialogflow.sync');
+
         Route::get('/dialogflow/intents', [AdminController::class, 'getDialogflowIntents'])->name('admin.dialogflow.intents');
         Route::get('/dialogflow/intents/active', [AdminController::class, 'getActiveDialogflowIntents'])->name('admin.dialogflow.intents.active');
         Route::post('/dialogflow/intents', [AdminController::class, 'createDialogflowIntent'])->name('admin.dialogflow.intents.create');
